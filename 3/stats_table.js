237f1d8b1b4482d81118bf236edbee9f6a49595e
realicraft (pi) <39655846+realicraft@users.noreply.github.com>
2021-09-04 23:53:02 -0700

    Turn 31
    - thins out lins on equipment
    - contrast improved in general
    - added <title>s to some pages

--- 3/stats_table.js
+++ 3/stats_table.js
@@ -17,30 +17,33 @@ var skilllist = [ //[name, css class for icon]
     ["Gathering", "gathering"]
 ]
 var equiplist = [ //[name, css class for icon, description, source, bonuses]
-    ["null", "iconless", "Whoops, forgot to put something here.", "An error", ""],
-    ["armor.empty_helm", "empty_helm", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_chest", "empty_chest", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_legs", "empty_legs", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_boots", "empty_boots", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_sword", "empty_sword", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_sheild", "empty_sheild", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_acc", "empty_acc", "desc.empty_equip", "src.empty_equip", ""],
-    ["armor.empty_food", "empty_food", "desc.empty_equip", "src.empty_equip", ""],
-    ["Wooden Sword", "wood_sword", "A sword made out of wood.", "Crafting", "+1 Indirect Attack"],
-    ["Wooden Shovel", "wood_shovel", "A shovel made out of wood. Not the most conventional of weapons.", "Crafting", "+1 Indirect Attack"],
-    ["Stick and Rock", "stick_rock", "A rock taped to a stick. This isn't very good.", "Crafting", "50% Chance of breaking on hit"]
+    ["null", "iconless", "Whoops, forgot to put something here.", "An error", ""], //0
+    ["armor.empty_helm", "empty_helm", "desc.empty_equip", "src.empty_equip", ""], //1
+    ["armor.empty_chest", "empty_chest", "desc.empty_equip", "src.empty_equip", ""], //2
+    ["armor.empty_legs", "empty_legs", "desc.empty_equip", "src.empty_equip", ""], //3
+    ["armor.empty_boots", "empty_boots", "desc.empty_equip", "src.empty_equip", ""], //4
+    ["armor.empty_sword", "empty_sword", "desc.empty_equip", "src.empty_equip", ""], //5
+    ["armor.empty_shield", "empty_shield", "desc.empty_equip", "src.empty_equip", ""], //6
+    ["armor.empty_acc", "empty_acc", "desc.empty_equip", "src.empty_equip", ""], //7
+    ["armor.empty_food", "empty_food", "desc.empty_equip", "src.empty_equip", ""], //8
+    ["Wooden Sword", "wood_sword", "A sword made out of wood.", "Crafting", "+1 Indirect Attack"], //9
+    ["Wooden Shovel", "wood_shovel", "A shovel made out of wood. Not the most conventional of weapons.", "Crafting", "+1 Indirect Attack"], //10
+    ["Stick and Rock", "stick_rock", "A rock taped to a stick. This isn't very good.", "Crafting", "50% Chance of breaking on hit"], //11
+    ["Golden Carrot", "gold_carrot", "A carrot made out of gold. Aren't carrots supposed to give you better eyesight?", "Gardening", "+0.3 Indirect Defense"], //12
+    ["Diamond Shield", "diamond_shield", "A shield with a diamond in the center.", "???", "+2 Indirect Defense<br />+1 Direct Defense"] //13
 ]
 var user_data = {
-    "realicraft":{"type":0,"health":[95,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[6,0,25]],"effects":[],"equips":[1,2,3,4,5,6,7,8]},
-    "SausageMcSauce":{"type":0,"health":[84,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8]},
-    "CatsUnited":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":3,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8]},
-    "Squrrelflight":{"type":0,"health":[97,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8]},
-    "IncendiaryGaming":{"type":0,"health":[70,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[7,2,13],[8,0,10],[4,0,20]],"effects":[],"equips":[1,2,3,4,11,6,7,8]},
-    "Byron_Inc_TBG":{"type":0,"health":[78,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":2,"skills":[[1,3,161]],"effects":[],"equips":[1,2,3,4,5,6,7,8]},
-    "cheesyfriedeggs":{"type":0,"health":[91,100],"attack":["8+3","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[[4,1,10],[1,0,77],[3,0,55]],"effects":[],"equips":[1,2,3,4,9,6,7,8]},
-    "solitare":{"type":0,"health":[86,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,60],[5,0,30],[3,0,65],[1,0,65]],"effects":[],"equips":[1,2,3,4,5,6,7,8]},
-    "Faressain":{"type":0,"health":[82,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8]},
-    "LeopardyLeaf":{"type":0,"health":[81,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,80]],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8]}
+    "realicraft":{"type":0,"health":[95,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[6,0,25]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
+    "SausageMcSauce":{"type":0,"health":[84,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
+    "CatsUnited":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":3,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
+    "Squrrelflight":{"type":0,"health":[97,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
+    "IncendiaryGaming":{"type":0,"health":[70,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[7,2,93],[8,0,10],[4,0,20]],"effects":[],"equips":[1,2,3,4,11,6,7,8,""]},
+    "Byron_Inc_TBG":{"type":0,"health":[78,100],"attack":["7+3","7+3"],"defense":["10+1",5],"kills":1,"deaths":2,"skills":[[1,4,24],[4,0,50]],"effects":[],"equips":[1,2,3,4,5,13,7,12,17]},
+    "cheesyfriedeggs":{"type":0,"health":[91,100],"attack":["8+3","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[[4,1,10],[1,0,77],[3,0,55]],"effects":[],"equips":[1,2,3,4,9,6,7,8,""]},
+    "solitare":{"type":0,"health":[86,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,60],[5,0,30],[3,0,65],[1,0,65]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
+    "Faressain":{"type":0,"health":[82,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
+    "LeopardyLeaf":{"type":0,"health":[81,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,80]],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
+    "gilbert_given_TBG":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[3,1,10]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]}
 }
 var stat_tables = ""
 var stat_table = ""
@@ -90,18 +93,22 @@ for (var i in user_data) {
         }
         stat_table += '</tbody></table>'
     }
-    if (user_data[i]["equips"].length != 0) {
-        stat_table += '<h3 style="margin: 0px;">Equipment</h3><span class="pt_equip">'
-        for (var l of user_data[i]["equips"]) {
-            stat_table += '<span class="icon '
-            stat_table += equiplist[l][1]
-            stat_table += '" onmouseover="tt('
-            stat_table += l
-            stat_table += ');" onmouseout="nt()"></span>'
-        }
-        stat_table += '</span>'
+    stat_table += '<h3 style="margin: 0px;">Equipment</h3><span class="pt_equip">'
+    for (var l of user_data[i]["equips"].slice(0,7)) {
+        stat_table += '<span class="icon '
+        stat_table += equiplist[l][1]
+        stat_table += '" onmouseover="tt('
+        stat_table += l
+        stat_table += ');" onmouseout="nt()"></span>'
     }
-    stat_table += "</div>"
+    stat_table += '<span class="icon '
+    stat_table += equiplist[user_data[i]["equips"][7]][1]
+    stat_table += '" onmouseover="tt('
+    stat_table += user_data[i]["equips"][7]
+    stat_table += ');" onmouseout="nt()" style="position:relative;"><span class="pt_stackcount">'
+    stat_table += user_data[i]["equips"][8]
+    stat_table += '</span></span>'
+    stat_table += '</span></div>'
     stat_tables += stat_table
 }
 var containEl = document.getElementById("stat_table_container")
