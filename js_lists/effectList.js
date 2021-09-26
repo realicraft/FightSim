f82e19486fc77c5209675c9bc70f1a4642b956da
realicraft (pi) <39655846+realicraft@users.noreply.github.com>
2021-09-25 23:03:51 -0700

    Turn 37

--- js_lists/effectList.js
+++ js_lists/effectList.js
@@ -5,6 +5,7 @@ var effectlist = [ //[name, css class for icon, description, source, bonuses]
     ["Frozen", "frozen", "Where'd all this ice come from, anyway?", "Ice", "Can't move"], //3
     ["KOd", "ko", "Maybe pay more attention to your HP next time?", "Reaching 0 HP", "Can't interact with anything<br />Can't be interacted with"], //4
     ["Paralyzed", "paralyzed", "Bzzzt!", "Electricity", "Actions have a 1/3 chance of failing"], //5
-    ["Poisoned", "poisoned", "\"Oooh, what's this purple stuff do?\"", "Poison", "-6 HP each turn"], //6
-    ["Soaked", "soaked", "You stayed in the rain too long and now you're all soggy.", "Rain", "You keep tracking water everywhere"] //7
+    ["Poisoned", "poisoned", "\"Oooh, what's this purple stuff do?\"", "Poison", "-6 to 3 HP each turn, depending on remaining duration"], //6
+    ["Soaked", "soaked", "You stayed in the rain too long and now you're all soggy.", "Rain", "You keep tracking water everywhere"], //7
+    ["Bleeding", "bleeding", "owie", "Taking damage", "-5 HP per turn<br />-1 Direct Attack"] //8
 ]
