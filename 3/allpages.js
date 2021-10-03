3948476c0503bf78db8da4f66fdb3e78b1604401
realicraft (pi) <39655846+realicraft@users.noreply.github.com>
2021-10-03 00:28:28 -0700

    Turn 39

--- 3/allpages.js
+++ 3/allpages.js
@@ -2,7 +2,7 @@ var headerEl = document.getElementById("header")
 var timeboxEl = document.getElementById("time_box")
 
 var headerLinks = [["Info", "info"],["Inventories", "inventories"],["Stats", "stats"],["Structures", "structures"],["Voting", "voting"],["Gardens", "gardens"]]
-var timeboxInfo = [38, "rain", "Downpour"]
+var timeboxInfo = [39, "cloudy", "Cloudy"]
 
 var headerCont = '<span class="small_icon fs3" style="position:absolute;left:4px;top:4px;" title="FS3"></span>'
 for (var i in headerLinks) {
--- js_lists/itemList.js
+++ js_lists/itemList.js
@@ -1,21 +1,23 @@
 var itemlist = [
-    ["lang.en.itemname.default", "lang.en.itemdesc.default", "lang.en.itemsrc.default", ""],
-    ["Potted Canned Bean Bush", "A bush that grows cans of beans. In a pot.", "Unknown", ""],
-    ["Logo 3", 'The "3" from the logo.', "Unknown", ""],
-    ["Logo F", 'The "F" from the logo.', "Unknown", ""],
-    ["Logo S", 'The "S" from the logo.', "Unknown", ""],
-    ["Rock", "It's a small rock.", "Picking up a rock", ""],
-    ["Coal", "It's a piece of coal.", "Mining", ""],
-    ["Diamond", "It's a diamond. These things are rare!", "Mining", ""],
-    ["Decent Hammer", "Bonk.", "Crafting", "+1 Direct Attack"],
-    ["Slingshot", "You shoot things with it.", "Unknown", "+1 Indirect Attack"],
-    ["Golden Ripe Spinach Bundle", "Golden <em>and</em> ripe!?", "Harvesting Spinach", "+1 HP per turn"],
-    ["Golden Potato", "Butter included.", "Harvesting Potatoes", "+1 Direct Attack"],
-    ["Flint & Steel", "Sets things on fire.", "Unknown", ""],
-    ["TNT", "Explodes after being ignited.", "Unknown", ""],
-    ["Icicle", "It's cold, and kinda pointy.", "Finding it", "+10% chance to Freeze on hit"],
-    ["Tray of Cookies", "A tray filled with cookies.", "Cooking", ""],
-    ["Log", "A log.", "Chopping", ""],
-    ["Stick", "It's a stick. You can use it in a bunch of different things.", "Chopping", ""],
-    ["Planks", "Some planks.", "Chopping", ""]
+    ["lang.en.itemname.default", "lang.en.itemdesc.default", "lang.en.itemsrc.default", ""], //0
+    ["Potted Canned Bean Bush", "A bush that grows cans of beans. In a pot.", "Unknown", ""], //1
+    ["Logo 3", 'The "3" from the logo.', "Unknown", ""], //2
+    ["Logo F", 'The "F" from the logo.', "Unknown", ""], //3
+    ["Logo S", 'The "S" from the logo.', "Unknown", ""], //4
+    ["Rock", "It's a small rock.", "Picking up a rock", ""], //5
+    ["Coal", "It's a piece of coal.", "Mining", ""], //6
+    ["Diamond", "It's a diamond. These things are rare!", "Mining", ""], //7
+    ["Decent Hammer", "Bonk.", "Crafting", "+1 Direct Attack"], //8
+    ["Slingshot", "You shoot things with it.", "Unknown", "+1 Indirect Attack"], //9
+    ["Golden Ripe Spinach Bundle", "Golden <em>and</em> ripe!?", "Harvesting Spinach", "+1 HP per turn"], //10
+    ["Golden Potato", "Butter included.", "Harvesting Potatoes", "+1 Direct Attack"], //11
+    ["Flint & Steel", "Sets things on fire.", "Unknown", ""], //12
+    ["TNT", "Explodes after being ignited.", "Unknown", ""], //13
+    ["Icicle", "It's cold, and kinda pointy.", "Finding it", "+10% chance to Freeze on hit"], //14
+    ["Tray of Cookies", "A tray filled with cookies.", "Cooking", ""], //15
+    ["Log", "A log.", "Chopping", ""], //16
+    ["Stick", "It's a stick. You can use it in a bunch of different things.", "Chopping", ""], //17
+    ["Planks", "Some planks.", "Chopping", ""], //18
+    ["Rotting Mush", "Eww, why did you keep this?", "Farming", "Get rid of it"], //19
+    ["Banana", "Potassium", "Unknown", "Eat it for potassium"] //20
 ]
