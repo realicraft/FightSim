c2f7096a843e9a964c05d6c58f8f5ea786e26202
realicraft (pi) <39655846+realicraft@users.noreply.github.com>
2021-10-07 00:39:42 -0700

    Turn 40
    - added some fruit
    - more ores (internally)

--- 3/stats_table.js
+++ 3/stats_table.js
@@ -1,16 +1,16 @@
 var typelist = ["typeless", "normal_type", "fire_type", "water_type", "elec_type", "plant_type", "ice_type", "light_type", "dark_type", "missingtype"]
 var user_data = {
-    "realicraft":{"type":0,"health":[65,100],"attack":["7+2","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[6,0,35]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
+    "realicraft":{"type":0,"health":[62,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[6,0,35]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
     "SausageMcSauce":{"type":0,"health":[84,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
     "CatsUnited":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":3,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
     "Squrrelflight":{"type":0,"health":[97,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
-    "IncendiaryGaming":{"type":0,"health":[61,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[7,3,128],[8,0,10],[4,0,30],[2,0,10]],"effects":[],"equips":[1,2,3,4,11,6,7,8,""]},
-    "Byron_Inc_TBG":{"type":0,"health":[78,100],"attack":["7+3","7+3"],"defense":["11+1",5],"kills":1,"deaths":2,"skills":[[1,4,236],[4,0,50],[7,1,115]],"effects":[],"equips":[1,2,3,4,5,13,7,12,21]},
+    "IncendiaryGaming":{"type":0,"health":[61,100],"attack":["8+3","7+3"],"defense":[5,5],"kills":1,"deaths":1,"skills":[[7,3,128],[8,0,10],[4,0,30],[2,1,73]],"effects":[],"equips":[1,2,3,4,78,6,7,8,""]},
+    "Byron_Inc_TBG":{"type":0,"health":[78,100],"attack":["7+3","7+3"],"defense":["10+1",5],"kills":1,"deaths":2,"skills":[[1,5,71],[4,0,50],[7,1,115],[8,1,40]],"effects":[],"equips":[1,2,3,4,5,13,7,12,21]},
     "cheesyfriedeggs":{"type":0,"health":[91,100],"attack":["7+4","7+3"],"defense":[5,5],"kills":0,"deaths":1,"skills":[[4,1,10],[1,1,119],[3,0,55]],"effects":[],"equips":[1,2,3,4,9,6,7,8,""]},
-    "solitare":{"type":0,"health":[86,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,60],[5,0,30],[3,0,65],[1,1,100]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
+    "solitare":{"type":0,"health":[86,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,60],[5,0,30],[3,0,65],[1,1,100],[7,0,90]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]},
     "Faressain":{"type":0,"health":[82,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
     "LeopardyLeaf":{"type":0,"health":[81,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[2,0,80]],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]},
-    "gilbert_given_TBG":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[3,1,10]],"effects":[],"equips":[1,2,3,4,5,6,7,8,""]}
+    "gilbert_given_TBG":{"type":0,"health":[100,100],"attack":["7+3","7+3"],"defense":[5,5],"kills":0,"deaths":0,"skills":[[3,1,10]],"effects":[[1,"*"]],"equips":[1,2,3,4,5,6,7,8,""]}
 }
 var stat_tables = ""
 var stat_table = ""
--- js_lists/equipList.js
+++ js_lists/equipList.js
@@ -12,7 +12,7 @@ var equiplist = [ //[name, css class for icon, description, source, bonuses]
     ["Wooden Shovel", "wood_shovel", "A shovel made out of wood. You should probably use this for digging instead.", "Crafting", "+1 Indirect Attack<br />+1 to Digging"], //10
     ["Stick and Rock", "stick_rock", "A rock taped to a stick. This isn't very good.", "Crafting", "50% Chance of breaking on hit"], //11
     ["Golden Carrot", "gold_carrot", "A carrot made out of gold. Aren't carrots supposed to give you better eyesight?", "Gardening", "+0.3 Indirect Defense"], //12
-    ["Diamond Shield", "diamond_shield", "A shield with a diamond in the center.", "???", "+2 Indirect Defense<br />+1 Direct Defense"], //13
+    ["Diamond Shield", "diamond_shield", "A shield with a diamond in the center.", "Crafting", "+1 Indirect Defense<br />+1 Direct Defense"], //13
     ["Golden Sword", "gold_sword", "A sword made out of gold. Seems fragile.", "Crafting", "+1 Indirect Attack<br />10% chance of breaking on hit"], //14
     ["Golden Shovel", "gold_shovel", "A shovel made out of gold. Seems fragile, and is probably btter used for digging.", "Crafting", "+1 Indirect Attack<br />+2 to Digging<br />10% chance of breaking on hit"], //15
     ["Golden Pickaxe", "gold_pickaxe", "A pickaxe made out of gold. Seems fragile, and is probably better used for mining.", "Crafting", "+1 Indirect Attack<br />+2 to Mining<br />10% chance of breaking on hit or mine"], //16
@@ -57,4 +57,25 @@ var equiplist = [ //[name, css class for icon, description, source, bonuses]
     ["Sandstone Bricks", "sandstone_bricks", "A cube of sandstone bricks. You could make a wall out of this.", "Crafting", ""], //55
     ["Carved Sandstone", "carved_sandstone", "A cube of carved sandstone. Nice carvings.", "Carving", ""], //56
     ["Glass", "glass_block", "A cube of glass. See-through, which makes up for its fragility.", "Crafting", ""], //57
+    ["Diamond Helmet", "diamond_helm", "A helmet made of diamond.", "Crafting", "+1 Direct Defense"], //58
+    ["Diamond Chestplate", "diamond_chest", "A chestplate made of diamond.", "Crafting", "+2 Direct Defense"], //59
+    ["Diamond Leggings", "diamond_legs", "A pair of leggings made of diamond.", "Crafting", "+1 Direct Defense<br />+1 Indirect Defense"], //60
+    ["Diamond Boots", "diamond_boots", "A pair of boots made of diamond.", "Crafting", "+1 Indirect Defense"], //61
+    ["Apple", "apple", "An apple.", "Farming", "+5 HP on consume"], //62
+    ["Golden Apple", "golden_apple", "A golden apple. One of the more common golden foods, and the only edible one.", "Farming", "Equip for +0.5 Max HP<br />Eat for Regen and Defense Up (1 turn)"], //63
+    ["Orange", "orange", "An orange. Not very similar to an apple, which is why they're compared so often.", "Farming", "+5 HP on consume"], //64
+    ["Golden Orange", "golden_orange", "A golden orange. No one knows whether it's golden on the inside, because the peel is solid gold and can't be removed.", "Farming", "+0.7 Max HP"], //65
+    ["Carrot", "carrot", "A carrot. It's orange.", "Farming", "+5 HP on consume<br />Consume like 10 at once to turn very slightly orange"], //66
+    ["Clay Shield", "clay_shield", "A shield made of clay. Try to avoid letting it shatter.", "Pottery", "+1 Indirect Defense<br />+1 Direct Defense<br />15% chance of breaking on hit"], //67
+    ["Iron Ore", "iron_ore", "Some iron ore. Can be crafted into bars, requiring 2 ore per bar.", "Mining", ""], //68
+    ["Copper Ore", "copper_ore", "Some copper ore. Can be crafted into bars, requiring 1 ore per bar.", "Mining", ""], //69
+    ["Gold Ore", "gold_ore", "Some gold ore. Can be crafted into bars, requiring 1 ore per bar.", "Mining", ""], //70
+    ["Gold Bar", "gold_bar", "A bar of solid gold. Actually kind of soft for a metal.", "Crafting", ""], //71
+    ["Iron Bar", "iron_bar", "A bar of solid iron. It's pretty sturdy.", "Crafting", ""], //72
+    ["Copper Bar", "copper_bar", "A bar of solid copper. Try to avoid getting it oxidized.", "Crafting", ""], //73
+    ["Water", "water", "One (1) level of water. Good for 0.5 turns of plant growth.", "Gathering", ""], //74
+    ["Emerald", "emerald", "It's an emerald. These things are rare!", "Mining", ""], //75
+    ["Hardite Ore", "hardite_ore", "Some hardite ore. Can be crafted into bars, requiring 3 ore per bar.", "Mining", ""], //76
+    ["Hardite Bar", "hardite_bar", "A bar of solid hardite, named after both how hard it is to get, and how hard it is as a material. This stuff can cut diamond!", "Crafting", ""], //77
+    ["Rock-on-a-Stick", "rock_on_stick", "A rock fastened to a stick with more rock. Better than just using tape.", "Crafting", "20% Chance of breaking on hit<br />+1 Indirect Attack"], //78
 ]
