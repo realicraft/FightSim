d36024dcdeffc265f499148e6051ac5c98b27892
realicraft <39655846+realicraft@users.noreply.github.com>
2023-06-21 03:01:05 -0700

    Turn 80 (addendum)
    - adds more file icons to file_sheet
    - updates the sitemap
    - (hopefully) fixes FightSnap! not loading the right url
    - removes an errant desktop.ini

--- Snap!/spawniframe.js
+++ Snap!/spawniframe.js
@@ -1,7 +1,7 @@
 var build_no = 0;
 
 var iframe_holder_el = document.getElementById("iframe_holder");
-var build_xml = new URL("/Snap!/builds/build " + build_no + ".xml", document.baseURI).href;
+var build_xml = new URL("/FightSim/Snap!/builds/build " + build_no + ".xml", document.baseURI).href;
 iframe_holder_el.innerHTML = '<iframe width="482" height="362" frameBorder=0 allowfullscreen allow="geolocation; microphone;camera" src="https://snap.berkeley.edu/run#run:' + build_xml +'&embedMode&noRun"></iframe>';
 
 var header_func = function() {
